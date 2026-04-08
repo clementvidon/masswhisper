7ebf3c10ed67cd57f6a0bf3242be600b69abb60b
cvidon <cvidon@student.42.fr>
2026-04-08 16:24:03 +0200

    refactor(terraform): remove unused outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,13 +1,9 @@
-output "public_api_domain" {
-  value = local.public_api_domain
-}
-
-output "server_name" {
-  value = hcloud_server.vm.name
-}
-
 output "server_ip" {
   value = hcloud_server.vm.ipv4_address
+}
+
+output "public_api_domain" {
+  value = local.public_api_domain
 }
 
 output "topic_runtime_env" {
